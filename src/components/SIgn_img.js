a19577fb469b81f416b2f008e2d4af07994e9a8f
Aryan raj <87406447+aryanraj4049@users.noreply.github.com>
2022-09-08 18:45:14 +0530

    Update SIgn_img.js

--- src/components/SIgn_img.js
+++ src/components/SIgn_img.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import boy from './sign.jpg'
 const SIgn_img = () => {
     return (
         <>
-            <div className="right_data mt-1" style={{width:850}}>
-                    <img src="giva.jpg"/>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="sign_img mt-5">
+                    <img src={boy}></img>
+                </div>
             </div>
         </>
     )
