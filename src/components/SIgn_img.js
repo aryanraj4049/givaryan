375b5afe4e1467a1de3242f2b1ea425bad59eaee
Aryan raj <87406447+aryanraj4049@users.noreply.github.com>
2022-09-08 16:02:00 +0530

    Update SIgn_img.js

--- src/components/SIgn_img.js
+++ src/components/SIgn_img.js
@@ -4,7 +4,7 @@ const SIgn_img = () => {
     return (
         <>
             <div className="right_data mt-1" style={{width:850}}>
-                    <img src="giva\public\giva.jpg"target="_blank" />
+                    <img src="giva.jpg"/>
             </div>
         </>
     )
